565c59e15dbdf20bdb69f2fde7441643f16d5912
kabirt7 <kabir.t7@hotmail.com>
2024-05-14 00:55:24 +1000

    updated backend link

--- to_do.backend/src/main/java/io/nology/todo_backend/config/WebConfig.java
+++ to_do.backend/src/main/java/io/nology/todo_backend/config/WebConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
-		String[] allowedOrigins = { "http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:3000", "http://localhost:9191", "golden-taiyaki-1890f8.netlify.app" };
+		String[] allowedOrigins = { "http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:3000", "http://localhost:9191", "https://golden-taiyaki-1890f8.netlify.app" };
 		registry.addMapping("/**").allowedOrigins(allowedOrigins)
 				.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS").allowedHeaders("*");
 	}
